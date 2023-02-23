ddc2e68bbf2bb1218f3da68b6d19fec30f0baa89
Robert Nicolescu <robertnicolescu42@yahoo.com>
2023-02-23 19:36:24 +0200

    Added station details modal

--- dissertation-frontend/dissertation/src/app/secure/station-details/station-details.component.ts
+++ dissertation-frontend/dissertation/src/app/secure/station-details/station-details.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnDestroy, OnInit } from '@angular/core';
+import { BsModalRef, BsModalService } from 'ngx-bootstrap/modal';
+// import { Workstation } from 'src/app/types/work-station';
+import { Workstation } from '../../../app/types/work-station';
 
 @Component({
   selector: 'app-station-details',
   templateUrl: './station-details.component.html',
-  styleUrls: ['./station-details.component.scss']
+  styleUrls: ['./station-details.component.scss'],
 })
-export class StationDetailsComponent {
+export class StationDetailsComponent implements OnInit, OnDestroy {
+  @Input()
+  workstation: Workstation | undefined;
 
+  constructor(
+    public bsModalRef: BsModalRef,
+    private modalService: BsModalService
+  ) {}
+
+  onClose() {
+    this.bsModalRef.hide();
+  }
+
+  ngOnDestroy(): void {}
+  ngOnInit(): void {}
 }
