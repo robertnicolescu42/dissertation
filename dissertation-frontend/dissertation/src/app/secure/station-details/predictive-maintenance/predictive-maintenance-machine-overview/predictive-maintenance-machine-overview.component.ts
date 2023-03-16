4fc870b7828dd05048e3cec356c9d7f9ec52a116
Robert Nicolescu <robertnicolescu42@yahoo.com>
2023-03-16 18:03:22 +0200

    DIS-18 - Tabs bugfixes

--- dissertation-frontend/dissertation/src/app/secure/station-details/predictive-maintenance/predictive-maintenance-machine-overview/predictive-maintenance-machine-overview.component.ts
+++ dissertation-frontend/dissertation/src/app/secure/station-details/predictive-maintenance/predictive-maintenance-machine-overview/predictive-maintenance-machine-overview.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Workstation } from '../../../../../app/types/work-station';
 
 @Component({
   selector: 'app-predictive-maintenance-machine-overview',
   templateUrl: './predictive-maintenance-machine-overview.component.html',
-  styleUrls: ['./predictive-maintenance-machine-overview.component.scss']
+  styleUrls: ['./predictive-maintenance-machine-overview.component.scss'],
 })
 export class PredictiveMaintenanceMachineOverviewComponent {
+  @Input()
+  workstation?: Workstation;
 
+  plantId: string;
+  stationId: string;
+  constructor(private route: ActivatedRoute) {
+    this.plantId = this.route.snapshot.paramMap.get('id') ?? '';
+    this.stationId = this.route.snapshot.paramMap.get('stationId') ?? '';
+  }
 }
