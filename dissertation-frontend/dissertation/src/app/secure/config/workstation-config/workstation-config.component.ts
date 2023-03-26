60b58d479e38fe13bd79b4144bdd25f1f98935a6
Robert Nicolescu <robertnicolescu42@yahoo.com>
2023-03-26 20:59:31 +0300

    DIS-22 - Started creating add/edit workstation

--- dissertation-frontend/dissertation/src/app/secure/config/workstation-config/workstation-config.component.ts
+++ dissertation-frontend/dissertation/src/app/secure/config/workstation-config/workstation-config.component.ts
@@ -1,10 +1,33 @@
 import { Component } from '@angular/core';
+import { WorkstationsService } from '../../services/workstations.service';
 
 @Component({
   selector: 'app-workstation-config',
   templateUrl: './workstation-config.component.html',
-  styleUrls: ['./workstation-config.component.scss']
+  styleUrls: ['./workstation-config.component.scss'],
 })
 export class WorkstationConfigComponent {
+  constructor(private workstationService: WorkstationsService) {}
 
+  workstation = {
+    plantIndex: '',
+    stationId: '',
+    displayName: '',
+    description: '',
+    cycleTime: 0,
+    runningTime: 0,
+    productionCount: 0,
+    defectCount: 0,
+    isOeeCalculable: false,
+    equipmentNumber: '',
+    cycleTimeDelay: 0,
+  };
+
+  onSubmit(): void {
+    // this.myService.addWorkstation(this.workstation);
+    console.log(this.workstation);
+    if (this.workstation) {
+      this.workstationService.addWorkstation(this.workstation);
+    }
+  }
 }
