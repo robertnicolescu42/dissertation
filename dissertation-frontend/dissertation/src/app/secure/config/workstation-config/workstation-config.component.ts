c3984d093c652bd865d163dfe06c9dd30ec09980
Robert Nicolescu <robertnicolescu42@yahoo.com>
2023-04-08 19:23:36 +0300

    DIS-22 - Added workstations counter in config

--- dissertation-frontend/dissertation/src/app/secure/config/workstation-config/workstation-config.component.ts
+++ dissertation-frontend/dissertation/src/app/secure/config/workstation-config/workstation-config.component.ts
@@ -24,6 +24,7 @@ export class WorkstationConfigComponent {
     cycleTimeDelay: 0,
   };
 
+
   onSubmit(): void {
     // this.myService.addWorkstation(this.workstation);
     console.log(this.workstation);
